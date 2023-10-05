c860a1f9a96985b2df9c0f373f22d8479d94e094
Laine Kranson <146883266+krannberry@users.noreply.github.com>
2023-10-04 17:08:21 -0700

    Add files via upload

---
+++ code-journal-1-1.py
@@ -0,0 +1,29 @@
+#this program will type my name, 
+#pronouns, favorite movie and food
+
+def PrintLaineKransonSheThey():
+	print("Laine Kranson, She/They")
+
+def main():
+	PrintLaineKransonSheThey()
+
+if __name__== "__main__":
+	main()
+
+def PrintMyfavoritemovieispracticalmagicItisaromcomaboutafamilyofwitches():
+	print("My favorite movie is practical magic.\nIt is a romcom about a family of witches.")
+
+def main():
+	PrintMyfavoritemovieispracticalmagicItisaromcomaboutafamilyofwitches()
+
+if __name__== "__main__":
+	main()
+
+def PrinyMyfavoritefoodissushiIlovesushirolls():
+	print("My favorite food is sushi.\nI love sushi rolls.")
+
+def main():
+	PrinyMyfavoritefoodissushiIlovesushirolls()
+
+if __name__== "__main__":
+	main()
