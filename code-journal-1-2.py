c8713338fcf45b8e6c9cb329ca5da08e84ae640d
Laine Kranson <146883266+krannberry@users.noreply.github.com>
2023-10-13 14:27:14 -0700

    Add files via upload

---
+++ code-journal-1-2.py
@@ -0,0 +1,17 @@
+def main():
+    a = 5.0
+    b = 3.0
+    c = 10
+    d = 4
+
+    e = a + b
+    print(f" sum {e}, {type(e)}")
+
+    f = c - d
+    print(f"difference {f}, {type(f)}")
+
+    g = a * d
+    print(f" product {g}, {type(g)}")
+
+if __name__ == '__main__':
+    main()
